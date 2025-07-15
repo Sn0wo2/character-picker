b85e2686afff4bc62b4267ae72993e3c1dea4d83
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-15 13:17:33 +0800

    fix: trace_id
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/handler/error/index.ts
+++ src/handler/error/index.ts
@@ -1,12 +1,12 @@
 import type {Context} from 'hono';
-import {md5} from "../../utils/hash-helper.ts";
+import {md5} from '../../utils/hash-helper.ts';
 
 export const errorHandler = async (err: Error, ctx: Context) => {
+    const hash = await md5(new TextEncoder().encode(`${err.name}: ${err.message}`));
+    const traceId = `${hash}-${Date.now()}`;
+
     return ctx.json({
         msg: 'oops, something went wrong',
-        data: {
-            trace_id: `${await md5(new TextEncoder().encode(`${err.name}: ${err.message}`))}-${new Date().getTime()}`
-        },
+        data: {trace_id: traceId},
     }, 500);
 };
-
