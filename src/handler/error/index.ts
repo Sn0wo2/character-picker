1db898ca55d7cbd93b12dfe1ab6a7023840c431a
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-07-15 14:47:39 +0800

    style: simple
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/handler/error/index.ts
+++ src/handler/error/index.ts
@@ -3,10 +3,9 @@ import {md5} from '../../utils/hash-helper.ts';
 
 export const errorHandler = async (err: Error, ctx: Context) => {
     const hash = await md5(new TextEncoder().encode(`${err.name}: ${err.message}`));
-    const traceId = `${hash}-${Date.now()}`;
 
     return ctx.json({
         msg: 'oops, something went wrong',
-        data: {trace_id: traceId},
+        data: {trace_id: `${hash}-${Date.now()}`},
     }, 500);
 };
