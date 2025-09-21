2623c26125f58c0bdb1aedc82e01e233e57f07e6
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-22 06:53:48 +0800

    refactor(middleware): cors
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,25 +1,16 @@
 import type {Context, Next} from 'hono';
 
-export const cors = async (c: Context, next: Next) => {
-    c.res.headers.set('Access-Control-Allow-Origin', '*');
-    c.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    c.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    c.res.headers.set('Access-Control-Max-Age', '86400');
+export const cors = async (ctx: Context, next: Next) => {
+    ctx.res.headers.set('Access-Control-Allow-Origin', '*');
+    ctx.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    ctx.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    ctx.res.headers.set('Access-Control-Max-Age', '86400');
 
-    if (c.req.method === 'OPTIONS') {
-        return new Response(
-            JSON.stringify({msg: 'success'}),
-            {
-                status: 204,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-                    'Access-Control-Max-Age': '86400',
-                },
-            }
-        );
+    if (ctx.req.method === 'OPTIONS') {
+        ctx.status(204)
+        return ctx.json({
+            msg: 'success'
+        })
     }
 
     await next();
