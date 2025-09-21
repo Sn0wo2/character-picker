57c4402b1d115d4d098bac2f7a8cae3dbf750e75
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-22 07:08:21 +0800

    fix(handler): error log
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/handler/error/index.ts
+++ src/handler/error/index.ts
@@ -2,10 +2,12 @@ import type {Context} from 'hono';
 import {md5} from '../../utils/hash-helper.ts';
 
 export const errorHandler = async (err: Error, ctx: Context) => {
-    const hash = await md5(new TextEncoder().encode(`${err.name}: ${err.message}`));
+    const traceID = `${await md5(new TextEncoder().encode(`${err.name}: ${err.message}`))}-${Date.now()}`
+
+    console.error(`TraceID: ${traceID}\n`, err)
 
     return ctx.json({
         msg: 'oops, something went wrong',
-        data: {trace_id: `${hash}-${Date.now()}`},
+        data: {trace_id: traceID},
     }, 500);
 };
