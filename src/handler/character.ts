9483354d273ba2f144b2ec64c98a1012630a84a4
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-23 16:50:37 +0800

    fix(handler): character custom header
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/handler/character.ts
+++ src/handler/character.ts
@@ -4,7 +4,8 @@ import {arrayBufferToBase64, parseImageDimensions} from "../utils/img-helper.ts"
 
 export const characterHandler = async (ctx: Context): Promise<Response> => {
     const url = new URL(ctx.req.url);
-    url.pathname = `/${getRandomCharacter(ctx.req.query().custom?.split(","))}`;
+    const character = getRandomCharacter(ctx.req.query().custom?.split(","))
+    url.pathname = `/${character}`;
 
     const assetResponse = await ctx.env.ASSETS.fetch(new Request(url.toString(), ctx.req.raw));
     if (!assetResponse.ok) return ctx.json({msg: "assets not found"}, 404);
@@ -34,7 +35,10 @@ export const characterHandler = async (ctx: Context): Promise<Response> => {
     <image class="img" x="0" y="0" ${dims?.width ? `width="${dims.width}"` : ""} ${dims?.height ? `height="${dims.height}"` : ""} href="data:${assetResponse.headers.get("Content-Type") || "application/octet-stream"};base64,${arrayBufferToBase64(buffer)}" preserveAspectRatio="xMidYMid meet" />
         </svg>`, {
         status: 200,
-        headers: {"content-type": "image/svg+xml; charset=utf-8"},
+        headers: {
+            "content-type": "image/svg+xml; charset=utf-8",
+            "x-character": character
+        },
     });
 };
 
