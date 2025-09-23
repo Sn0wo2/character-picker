08eef4bdacca89944a570e269a46aeeb7f50578a
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-23 17:04:17 +0800

    fix(middleware): new cache rule
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,7 +1,7 @@
 import type {Context, Next} from 'hono';
 
 export const cache = async (c: Context, next: Next) => {
-    c.res.headers.set('Cache-Control', 'no-cache');
+    c.res.headers.set('Cache-Control', 'no-store, no-cache');
     
     await next();
 };
