aa6cc451a886b3f63b26bf617df74abe48ae396d
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-23 17:09:34 +0800

    style(middleware): cleanup
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -2,6 +2,6 @@ import type {Context, Next} from 'hono';
 
 export const cache = async (c: Context, next: Next) => {
     c.res.headers.set('Cache-Control', 'no-store, no-cache');
-    
+
     await next();
 };
