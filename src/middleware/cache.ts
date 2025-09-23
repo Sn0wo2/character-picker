072862cb7c912b355918cf5bfd7d14213e7aa64b
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-23 17:00:32 +0800

    fix(middleware): test new cache rule
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,11 +1,7 @@
 import type {Context, Next} from 'hono';
 
 export const cache = async (c: Context, next: Next) => {
-    c.res.headers.set(
-        'Cache-Control',
-        'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
-    );
-    c.res.headers.set('Pragma', 'no-cache');
-    c.res.headers.set('Expires', '0');
+    c.res.headers.set('Cache-Control', 'no-cache');
+    
     await next();
 };
