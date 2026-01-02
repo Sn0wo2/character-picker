a8091be629fe3d25f74a12a565f6b93d004f0a3b
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-01-03 03:45:02 +0800

    feat(router): make character handler primal router
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,15 @@ import {Hono} from 'hono';
 import {charactersHandler} from '../handler/api-characters';
 import {characterHandler} from '../handler/character';
 import {errorHandler} from '../handler/error';
-import {notFoundHandler} from '../handler/notfound';
 import {cache} from '../middleware/cache';
 import {cors} from '../middleware/cors';
 
 export const setupRouter = (app: Hono) => {
     app.onError(errorHandler);
-    app.notFound(notFoundHandler);
+    app.notFound(characterHandler);
 
     app.use(cache);
     app.use(cors);
-
-    app.get('/character', characterHandler);
 
     const test = new Hono(); // v0 debug
     const api = new Hono(); // v1 release
