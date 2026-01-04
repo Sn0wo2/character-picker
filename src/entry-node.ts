1e639643bd511ea2bf1aea38b754a643b389872f
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-01-05 01:53:17 +0800

    refactor(node): import
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/entry-node.ts
+++ src/entry-node.ts
@@ -1,6 +1,8 @@
 import {Hono} from 'hono'
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import mime from 'mime-types';
+import {serve} from "@hono/node-server";
 
 try {
     globalThis.CHARACTER_INDEX = await fs.readFile(path.join(process.cwd(), 'public', '.index'), 'utf-8');
@@ -21,19 +23,10 @@ const assetsFetcher = {
         const pathname = url.pathname.replace(/^\//, '');
         const filePath = path.join(process.cwd(), 'public', pathname);
 
-        const stats = await fs.stat(filePath);
-        if (stats.isFile()) {
-            const data = await fs.readFile(filePath);
-            const ext = path.extname(filePath).toLowerCase();
-            let type = 'application/octet-stream';
-            if (ext === '.png') type = 'image/png';
-            else if (ext === '.jpg' || ext === '.jpeg') type = 'image/jpeg';
-            else if (ext === '.json') type = 'application/json';
-            else if (ext === '.txt') type = 'text/plain';
-            else if (ext === '.html') type = 'text/html';
+        if (await fs.stat(filePath)) {
 
-            return new Response(data, {
-                headers: {'Content-Type': type},
+            return new Response(await fs.readFile(filePath), {
+                headers: {'Content-Type': mime.lookup(filePath) || 'application/octet-stream'},
             });
         }
         return new Response('Not Found', {status: 404});
@@ -53,9 +46,6 @@ setupRouter(app);
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
-
-// dynamic import!
-const {serve} = await import('@hono/node-server');
 
 serve({
     fetch: app.fetch,
