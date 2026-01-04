a96909cea5d3e672b98647201c16db3c49e5a5b3
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-01-05 01:53:35 +0800

    style(middleware): format
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,16 +1,16 @@
 import type {Context, Next} from 'hono';
 
 export const cors = async (ctx: Context, next: Next) => {
-  ctx.res.headers.set('Access-Control-Allow-Origin', '*');
-  ctx.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  ctx.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  ctx.res.headers.set('Access-Control-Max-Age', '86400');
+    ctx.res.headers.set('Access-Control-Allow-Origin', '*');
+    ctx.res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    ctx.res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    ctx.res.headers.set('Access-Control-Max-Age', '86400');
 
-  if (ctx.req.method === 'OPTIONS') {
-    return ctx.json({
-      msg: 'success'
-    }, 200)
-  }
+    if (ctx.req.method === 'OPTIONS') {
+        return ctx.json({
+            msg: 'success'
+        }, 200)
+    }
 
-  await next();
+    await next();
 };
