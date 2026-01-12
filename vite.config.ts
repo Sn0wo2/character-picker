49d6d4c52fbe703bc1a2df4a39cf88f813109728
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-01-12 13:14:24 +0800

    chore(vite): update build output dist
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import {cloudflare} from '@cloudflare/vite-plugin';
 import {defineConfig} from 'vite';
-import ssrPlugin from 'vite-ssr-components/plugin';
 import fs from 'node:fs';
 import path from 'node:path';
 
 export default defineConfig({
-    plugins: [cloudflare(), ssrPlugin()],
+    plugins: [
+        cloudflare()
+    ],
     define: {
         'globalThis.CHARACTER_INDEX': JSON.stringify(fs.readFileSync(path.join(process.cwd(), 'public', '.index'), 'utf-8')),
         'globalThis.VERSION': JSON.stringify(fs.readFileSync(path.join(process.cwd(), 'public', '.version'), 'utf-8')),
     },
+    environments: {
+        character_picker: {
+            build: {
+                emptyOutDir: true
+            }
+        }
+    },
     build: {
-        rollupOptions: {
-            input: {
-                index: './src/index.ts',
-            },
-            output: {
-                entryFileNames: '_worker.js',
-            },
-        },
+        minify: true,
     },
 });
